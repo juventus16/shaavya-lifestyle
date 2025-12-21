3f594d7968bd22b3af376b6f6d05fa7e00ff1231
Aditya <adityashm03@gmail.com>
2025-12-21 17:30:13 +0530

    Added Footer Component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  year = new Date().getFullYear();
 }
